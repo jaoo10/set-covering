05674faf3652c9f51715df15659b8503eb63d862
João Almeida Jr <joao29a@gmail.com>
2013-11-05 14:03:45 -0200

    selection implementado

--- setCovering.py
+++ setCovering.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python
 
-import sys
-import random
-import copy
-import time
+from sys import argv
+from random import seed, sample
+from copy import deepcopy
+from time import time
 
-class Subset:
-    def __init__(self,cost):
-        self.cost = cost
-        self.elements = set()
-
-    def setSubElement(self,element):
-        self.elements.add(element)
-
-class SetUniverse:
-    def __init__(self,setCover,setElements):
-        self.setCover = setCover
-        self.setElements = setElements
-
-def createSet(filename):
+#read the file and return a list with the universe and the subsets
+def readFile(filename):
     isData = False
-    setCover = dict()
-    setElements = set()
+    subsets = dict()
+    universe = set()
     f = open(filename)
     for line in f:
         data = line.strip("\n").split()
@@ -30,40 +18,47 @@ def createSet(filename):
             if dataType == "dados" or dataType == "densidade":
                 isData = True
         else:
-            setCover[data[0]] = Subset(float(data[1]))
+            subsets[data[0]] = [float(data[1]),set()]
             for i in range(2,len(data)):
-                setElements.add(data[i])
-                setCover[data[0]].setSubElement(data[i])
+                universe.add(data[i])
+                subsets[data[0]][1].add(data[i])
     f.close()
-    return SetUniverse(setCover,setElements)
+    return universe, subsets
 
 def generatePopulation(universe,subset,maxSize):
     population = list()
     while (len(population) < maxSize):
         solution = set()
-        universeTemp = copy.deepcopy(universe)
+        universeTemp = deepcopy(universe)
+        cost = 0
         while (len(universeTemp) != 0):
-            row = random.sample(universeTemp,1)
+            row = sample(universeTemp,1).pop()
             for i in subset:
-                if row[0] in subset[i].elements:
+                if row in subset[i][1]:
                     solution.add(i)
-                    universeTemp.difference_update(subset[i].elements)
-        population.append(solution)
+                    cost += subset[i][0]
+                    universeTemp.difference_update(subset[i][1])
+        population.append([solution,cost])
     return population
 
+def selection(population):
+    individuals = list()
+    for i in range(0,3): individuals.append(sample(population,1).pop())
+    return min(individuals, key = lambda individual: individual[1])
+
 if __name__ == "__main__":
-    random.seed(time.time())
-    filename = sys.argv[1]
-    setUniverse = createSet(filename)
-    for i in setUniverse.setCover:
-        print("%s:" % i),
-        print(setUniverse.setCover[i].cost),
-        print(setUniverse.setCover[i].elements)
-    population = generatePopulation(setUniverse.setElements,
-            setUniverse.setCover,int(sys.argv[2]))
-    for subset in population:
-        cost = 0
-        for col in subset:
-            cost += setUniverse.setCover[col].cost
-        print(subset),
-        print(cost)
+    seed(time())
+    filename = argv[1]
+    universe, subsets = readFile(filename)
+    for i in subsets: # get the subsets
+        print(i), #print the column (subset)
+        print(subsets[i][0]), #the cost of the column
+        print(subsets[i][1])  #the elements of the column
+    print(universe)
+    print
+    population = generatePopulation(universe,subsets,int(argv[2]))
+    individual = selection(population)
+    for solution in population:
+        print(solution)
+    print
+    print(individual)
